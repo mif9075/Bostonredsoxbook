a39bd20bb8e7d88cf8b85a530672d49c5aad8995
Miguel Fernandez <miguel.fernandez@codeimmersives.com>
2019-03-27 13:01:09 -0400

    Added all Red Sox Nation Pictures

--- data.js
+++ data.js
@@ -1,3 +1,130 @@
 const data = [
-    
+    {
+        position : 'pitcher',
+        number : 32,		
+        name : 'Matt Barnes',
+        bat : 'right',
+        throw : 'right',
+        },
+    {
+        position : 'pitcher',
+        number : 70,		
+        name : 'Ryan Brasier',
+        bat : 'right',
+        throw : 'right',
+    },
+    {
+        position : 'pitcher',
+        number : 72,		
+        name : 'Colten Brewer',
+        bat : 'right',
+        throw : 'right',
+    },
+    {
+        position : 'pitcher',
+        number : 17,		
+        name : 'Nathan Eovaldi',
+        bat : 'right',
+        throw : 'right',
+    },
+    {
+        position : 'pitcher',
+        number : 37,		
+        name : 'Heath Hembree',
+        bat : 'right',
+        throw : 'right',
+    },
+    {
+        position : 'pitcher',
+        number : 61,		
+        name : 'Brian Johnson',
+        bat : 'left',
+        throw : 'left',
+    },
+    {
+        position : 'pitcher',
+        number : 22,		
+        name : 'Rick Porcello',
+        bat : 'right',
+        throw : 'right',
+    },
+    {
+        position : 'pitcher',
+        number : 10,		
+        name : 'David Price',
+        bat : 'left',
+        throw : 'left',
+    },
+    {
+        position : 'pitcher',
+        number : 57,		
+        name : 'Eduardo Rodriguez',
+        bat : 'left',
+        throw : 'left',
+    },
+    {
+        position : 'pitcher',
+        number : 41,		
+        name : 'Chris Sale',
+        bat : 'left',
+        throw : 'left',
+    },
+    {
+        position : 'pitcher',
+        number : 47,		
+        name : 'Tyler Thornburg',
+        bat : 'right',
+        throw : 'right',
+    },
+    {
+        position : 'pitcher',
+        number : 76,		
+        name : 'Hector Velazquez',
+        bat : 'right',
+        throw : 'right',
+    },
+    {
+        position : 'pitcher',
+        number : 44,		
+        name : 'Brandon Workman',
+        bat : 'right',
+        throw : 'right',
+    },
+    {
+        position : 'pitcher',
+        number : 35,		
+        name : 'Steven Wright',
+        bat : 'right',
+        throw : 'right',
+    },
+
+{position: catchers
+
+#	 	Name	B/T	Ht	Wt	DOB
+23		Blake Swihart	S/R	6'1"	200lbs	4/3/92
+7		Christian Vazquez	R/R	5'9"	195lbs	8/21/90
+},
+
+{position: infield
+
+    #	 	Name	B/T	Ht	Wt	DOB
+    2		Xander Bogaerts	R/R	6'1"	210lbs	10/1/92
+    11		Rafael Devers	L/R	6'0"	237lbs	10/24/96
+    40		Marco Hernandez	L/R	6'0"	200lbs	9/6/92
+    12		Brock Holt	L/R	5'10"	180lbs	6/11/88
+    18		Mitch Moreland	L/L	6'2"	230lbs	9/6/85
+    36		Eduardo Nunez	R/R	6'0"	195lbs	6/15/87
+    25		Steve Pearce	R/R	5'11"	200lbs	4/13/83
+    15		Dustin Pedroia	R/R	5'9"	175lbs	8/17/83
+    59		Sam Travis	R/R	6'0"	205lbs	8/27/93
+
+},
+{position: outfield
+    16		Andrew Benintendi	L/L	5'10"	170lbs	7/6/94
+    50		Mookie Betts	R/R	5'9"	180lbs	10/7/92
+    19		Jackie Bradley Jr.	L/R	5'10"	200lbs	4/19/90
+    28		J.D. Martinez	R/R	6'3"	220lbs	8/21/87
+
+
+},    
 ]
