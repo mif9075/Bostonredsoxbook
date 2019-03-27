13de9d75f7416904e98237508cedaf8c8fe88432
Miguel Fernandez <miguel.fernandez@codeimmersives.com>
2019-03-27 14:20:21 -0400

    Added initialize functions

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 window.onload = init;
+
+function init(){
+    addClick('#search', search);
+    addClick('#display-all', displayAll);
+}
+
+function search(){
+}
+
+function displayAll(){
+    
+}
+
+
+//Helper Function
+function addClick(selector, func) {
+    document.querySelector(selector)
+        .addEventListener('click', func);
+}
